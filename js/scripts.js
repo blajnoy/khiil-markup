d835302663beef130eb442c48a1cb1872997e1c5
blajnoy <blajnoy@gmail.com>
2018-05-30 07:53:23 +0300

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 var $dropzone = null;
 
 Dropzone.options.contactForm = {
-    url: 'fake',
     autoProcessQueue: false,
     uploadMultiple: true,
     maxFiles: 10,
@@ -10,47 +9,7 @@ Dropzone.options.contactForm = {
     autoDiscover: false,
     maxFilesize: 25,
     previewsContainer: '#dropzonePreview',
-    clickable:'#dropzonePreview',
-    init: function () {
-        $dropzone = this;
-        this.options.url = "";
-        /*$('#lets_do_this').click(function(e){
-         e.preventDefault();
-         e.stopPropagation();
-         if (!validator.isValid){
-         setInputState('name');
-         //alert(validator.emailIsValid);
-         setInputState('email', !validator.emailIsValid);
-         setInputState('website');
-         setInputState('description');
-         inputStatesSet=true;
-
-         return;
-
-         }
-         else {
-
-         if (!$dropzone.getAcceptedFiles().length){
-         //document.forms[0].submit();
-         var formData = new FormData();
-         formData.append("name", $('#name').val());
-         formData.append("email", $('#email').val());
-         formData.append("website", $('#website').val());
-         formData.append("description", $('#description').val());
-         var req = new XMLHttpRequest();
-         req.open("POST","");
-         req.send(formData);
-         }
-         else {
-         $dropzone.processQueue();
-         }
-         setTimeout(function(){
-         $('#success_container').show();
-         $('#my-form').hide();
-         }, 3000);
-         }
-         });*/
-    }
+    clickable:'#dropzonePreview'
 };
 function fullpageInit() {
     $('#fullpage').fullpage();
@@ -58,5 +17,21 @@ function fullpageInit() {
 
 jQuery(document).ready(function () {
     fullpageInit();
+
+
+    /*--- Mobile menu ------------------------------------------*/
+    if ( $(window).width() < 768 ) {
+
+        if (!$('.fp-enabled').length) {
+            $.fn.fullpage.destroy('all');
+            $('body').addClass('mobile-disabled-fullpage');
+        }
+
+        $('.mobile-nav-toggle').on('click', function () {
+            $(this).toggleClass('active');
+            $('.navbar').toggleClass('active');
+        });
+
+    }
 });
 
